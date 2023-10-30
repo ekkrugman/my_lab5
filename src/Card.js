67f321b43b00f3b6a3bfac1a9cf6e6c78ded9ad6
ekkrugman <ekkrugman@mix.wvu.edu>
2023-10-29 21:52:08 -0400

    finished lab6

--- src/Card.js
+++ src/Card.js
@@ -5,37 +5,36 @@ function Card() {
         <div class = "container">
             <div class = "row">
                 <div class = "col">
-   
-        <div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <div class="card border-success mb-3">
+  <div class="card-header bg-transparent border-success">Header</div>
+  <div class="card-body text-success">
+    <h5 class="card-title">Success card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
   </div>
+  <div class="card-footer bg-transparent border-success">Footer</div>
 </div>
 </div>
 
 <div class = "col">
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+<div class="card border-success mb-3" >
+  <div class="card-header bg-transparent border-success">Header</div>
+  <div class="card-body text-success">
+    <h5 class="card-title">Success card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
   </div>
+  <div class="card-footer bg-transparent border-success">Footer</div>
 </div>
 </div>
 
 <div class = "col">
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+<div class="card border-success mb-3">
+  <div class="card-header bg-transparent border-success">Header</div>
+  <div class="card-body text-success">
+    <h5 class="card-title">Success card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
   </div>
-  </div>
+  <div class="card-footer bg-transparent border-success">Footer</div>
+</div>
 </div>
 </div>
 </div>
